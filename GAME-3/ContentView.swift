9e0e00e94c233cf7ae4cbc710c05cb4ee81d0bc0
jubao <tqy_4896@hotmail.com>
2021-03-31 10:12:30 +0800

    Initial Commit

---
+++ GAME-3/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GAME-3
+//
+//  Created by User16 on 2021/3/31.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GAME-3/GAME_3App.swift
@@ -0,0 +1,17 @@
+//
+//  GAME_3App.swift
+//  GAME-3
+//
+//  Created by User16 on 2021/3/31.
+//
+
+import SwiftUI
+
+@main
+struct GAME_3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
